0d3f5623e395c0427ec38e760b78cac6ab82d795
HassanShanJava <hassanshan675@gmail.com>
2022-09-19 05:38:35 +0500

    finished hero, working on cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Cards from "./components/Cards";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Hero/>
+      <Cards/>
     </div>
   );
 }
